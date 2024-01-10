29ac1defac9cbdfe65e1b70e5b2874c0ec275246
stalin16 <stalin16112003@gmail.com>
2024-01-10 15:29:38 +0700

    java exam 2

---
+++ Main.java
@@ -0,0 +1,32 @@
+class Rectangle {
+    protected double length;
+    protected double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getArea() {
+        return length * width;
+    }
+
+    public double getPerimeter() {
+        return 2 * (length + width);
+    }
+}
+
+class Square extends Rectangle {
+
+    public Square(double side) {
+        super(side, side);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Square square = new Square(10.0);
+        System.out.println("Square Area: " + square.getArea());
+        System.out.println("Square Perimeter: " + square.getPerimeter());
+    }
+}
