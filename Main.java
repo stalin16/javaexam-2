f032e25096b8a50c71bd9490178d3dd6f6111b3d
stalin16 <stalin16112003@gmail.com>
2024-01-10 15:42:47 +0700

    java exam 2

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Rectangle {
-    protected double length;
-    protected double width;
+    private double length;
+    private double width;
 
     public Rectangle(double length, double width) {
         this.length = length;
